0bf5e81fd387530024f4cd4d006bcf3e5be97c94
MrNobody144 <ljshin15@gmail.com>
2025-06-29 17:46:00 +0800

    Added new transform function that is called during sphere_its function

--- notes/ray.c
+++ notes/ray.c
@@ -4,7 +4,7 @@ t_ray	*ray(t_tuple *origin, t_tuple *direction)
 {
 	t_ray	*new_ray;
 
-	new_ray = calloc(1, sizeof(t_ray *));
+	new_ray = calloc(1, sizeof(t_ray));
 	if (!new_ray)
 		return (NULL);
 	new_ray->ori = origin;
@@ -22,7 +22,6 @@ t_tuple	*travel(t_ray *ray, double time)
 	new_pos = add(ray->ori->val, velo, ray->ori->size);
 	new_t_pos = tuple(0.0);
 	new_t_pos->val = new_pos;
-	new_t_pos->val = new_pos;
 	new_t_pos->size = ray->ori->size;
 	free(velo);
 	return (new_t_pos);
@@ -36,10 +35,9 @@ t_its	*sphere_its(t_ray *r, t_sphere *sphere)
 	double		rtc;
 	double		h;
 
-	obj = calloc(1, sizeof(t_obj *));
+	obj = object(sphere, 's');
 	if (!obj)
 		return (NULL);
-	obj->sphere = sphere;
 	rto = sub(sphere->ori->val, r->ori->val, r->ori->size);
 	rtc = dot(rto, r->dir->val, r->dir->size);
 	h = pow(mag(rto, r->ori->size), 2) - pow(rtc, 2);
@@ -47,13 +45,30 @@ t_its	*sphere_its(t_ray *r, t_sphere *sphere)
 		return (free(rto), its(obj, NULL, 0));
 	result = calloc(2, sizeof(double));
 	if (!result)
-		return (free(rto), NULL);
+		return (free(rto), free(obj), NULL);
 	h = sqrt(pow(sphere->rad, 2) - h);
 	result[0] = rtc - h;
 	result[1] = rtc + h;
 	if (result[0] == result[1])
 		return (free(rto), its(obj, result, 1));
 	return (free(rto), its(obj, result, 2));
+}
+
+void	transform(t_ray *ray, t_tuple **t_matrix)
+{
+	t_tuple	**temp_m;
+	t_tuple	**result_m;
+
+	temp_m = matrix(2, ray->ori);
+	result_m = mxm(temp_m, inverse(t_matrix));
+	ray->ori = result_m[0];
+	free_m(temp_m, temp_m[0]->size);
+	free(result_m);
+	temp_m = matrix(2, ray->dir);
+	result_m = mxm(temp_m, inverse(t_matrix));
+	ray->dir = result_m[0];
+	free_m(temp_m, temp_m[0]->size);
+	free(result_m);
 }
 
 void	free_ray(t_ray *ray)
