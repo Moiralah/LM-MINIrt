10e123be00214c2682176e299b5eb3bf679d30bf
MrNobody144 <ljshin15@gmail.com>
2025-07-04 14:07:48 +0800

    Norminated the lighting function

--- notes/shading.c
+++ notes/shading.c
@@ -1,111 +1,69 @@
 #include "minirt.h"
 
-t_tuple	*reflect(t_tuple *in, t_tuple *normal)
+t_mat	*material(int color, t_tuple *values)
 {
-	double	dot_product;
+	t_mat	*material;
 
-	dot_product = dot(in->val, normal->val, 4);
-	return (sub(in->val, mult(normal->val, 2 * dot_product, 4), 4));
-}
-
-t_light	*light(t_tuple *position, int intensity)
-{
-	t_light	*light;
-
-	light = malloc(sizeof(t_light));
-	if (!light)
-		return (NULL);
-	light->position = position;
-	light->intensity = intensity;
-	return (light);
-}
-
-t_material	*material(int color, double ambient, double diffuse, double specular, double shininess)
-{
-	t_material	*material;
-
-	material = malloc(sizeof(t_material));
+	material = malloc(sizeof(t_mat));
 	if (!material)
 		return (NULL);
 	material->color = color;
-	material->ambient = ambient;
-	material->diffuse = diffuse;
-	material->specular = specular;
-	material->shininess = shininess;
+	material->ambient = values->val[0];
+	material->diffuse = values->val[1];
+	material->specular = values->val[2];
+	material->shininess = values->val[3];
 	return (material);
 }
 
-// int lighting(t_material *material, t_light *light, t_tuple *point, t_tuple *eye, t_tuple *normal)
-// {
-// 	double	effective_color;
-// 	double	*lightv;
-// 	double	ambient;
-// 	double	light_dot_normal;
-// 	double	diffuse;
-// 	double	specular;
-// 	double	reflect_dot_eye;
-// 	double	factor;
-// 	t_tuple	*reflectv;
-
-// 	effective_color = material->color * light->intensity;
-// 	lightv = norm(sub(light->position, point->val, 4), 4);
-// 	ambient = effective_color * material->ambient;
-// 	light_dot_normal = dot(lightv, normal->val, 4);
-// 	if (light_dot_normal < 0)
-// 	{
-// 		diffuse = 0.0;
-// 		specular = 0.0;
-// 	}
-// 	else
-// 		diffuse = effective_color * material->diffuse * light_dot_normal;
-// 	reflectv = reflect(mult(lightv, -1.0, 4), normal);
-// 	reflect_dot_eye = dot(reflectv->val, eye->val, 4);
-// 	if (reflect_dot_eye <= 0)
-// 		specular = 0.0;
-// 	else
-// 	{
-// 		factor = pow(reflect_dot_eye, material->shininess);
-// 		specular = light->intensity * material->specular * factor;
-// 	}
-// 	return (ambient + diffuse + specular);
-// }
-
-double	specula(t_tuple *reflectv, t_tuple *eye, t_material *material, t_light *light)
+double	spclr(t_tuple *reflect, t_tuple *eye, t_mat *mat, t_light *light)
 {
 	double	reflect_dot_eye;
 	double	factor;
 	double	specular;
 
-	reflect_dot_eye = dot(reflectv->val, eye->val, 4);
+	reflect_dot_eye = dot(reflect, eye);
 	if (reflect_dot_eye <= 0)
 		specular = 0.0;
 	else
 	{
-		factor = pow(reflect_dot_eye, material->shininess);
-		specular = light->intensity * material->specular * factor;
+		factor = pow(reflect_dot_eye, mat->shininess);
+		specular = light->intensity * mat->specular * factor;
 	}
 	return (specular);
 }
 
-int	lighting(t_material *material, t_light *light, t_tuple *point, t_tuple *eye, t_tuple *normal)
+void	diffuse(t_mat *mat, t_tuple **m, t_tuple **temp, double *values)
 {
-	double	effective_color;
-	double	*lightv;
-	double	light_dot_normal;
-	double	diffuse;
-	double	specular;
+	if (values[1] >= 0)
+		values[2] = values[0] * mat->diffuse * values[1];
+	free_t(temp[0]);
+	temp[0] = mult(temp[1], -1.0);
+	if (!temp[0])
+		return (free_t(temp[1]));
+	free_t(temp[1]);
+	temp[1] = reflect(temp[0], m[2]);
+	if (!temp[1])
+		return (free_t(temp[0]));
+	free_t(temp[0]);
+}
 
-	effective_color = material->color * light->intensity;
-	lightv = norm(sub(light->position, point->val, 4), 4);
-	light_dot_normal = dot(lightv, normal->val, 4);
-	if (light_dot_normal < 0)
-	{
-		diffuse = 0.0;
-		specular = 0.0;
-	}
-	else
-		diffuse = effective_color * material->diffuse * light_dot_normal;
-	specular = specula(reflect(mult(lightv, -1.0, 4), normal),
-			eye, material, light);
-	return ((effective_color * material->ambient) + diffuse + specular);
+int	lighting(t_mat *mat, t_light *light, t_tuple **m)
+{
+	t_tuple	*temp[2];
+	double	values[4];
+
+	temp[0] = sub(light->position, m[0]);
+	if (!temp[0])
+		return (-1);
+	temp[1] = norm(temp[0]);
+	if (!temp[1])
+		return (free_t(temp[0]), -1);
+	values[0] = mat->color * light->intensity;
+	values[1] = dot(temp[1], m[2]);
+	values[2] = 0.0;
+	values[3] = 0.0;
+	diffuse(mat, m, temp, values);
+	values[3] = spclr(temp[1], m[1], mat, light);
+	free_t(temp[1]);
+	return ((values[0] * mat->ambient) + values[2] + values[3]);
 }
