fd48761a544cbea5ba042d4cb8bb69c5eb40ca2a
MrNobody144 <ljshin15@gmail.com>
2025-07-04 14:06:39 +0800

    Fixed errors related to incorrect tuple function calling

--- notes/ray.c
+++ notes/ray.c
@@ -17,45 +17,42 @@ t_ray	*ray(t_tuple *origin, t_tuple *direction)
 t_tuple	*travel(t_ray *ray, double time)
 {
 	t_tuple	*new_t_pos;
-	double	*velo;
-	double	*new_pos;
+	t_tuple	*velo;
+	t_tuple	*new_pos;
 
-	velo = mult(ray->dir->val, time, ray->dir->size);
-	new_pos = add(ray->ori->val, velo, ray->ori->size);
-	new_t_pos = tuple(0.0);
-	new_t_pos->val = new_pos;
-	new_t_pos->size = ray->ori->size;
-	free(velo);
-	return (new_t_pos);
+	velo = mult(ray->dir, time);
+	new_pos = add(ray->ori, velo);
+	free_t(velo);
+	return (new_pos);
 }
 
 // Computes the intersections of a ray with a sphere.
 t_its	*sphere_its(t_ray *r, t_sphere *sphere)
 {
 	t_obj		*obj;
+	t_tuple		*rto;
 	double		*result;
-	double		*rto;
 	double		rtc;
 	double		h;
 
 	obj = object(sphere, 's');
 	if (!obj)
 		return (NULL);
-	transform(r, sphere->transform);
-	rto = sub(sphere->ori->val, r->ori->val, r->ori->size);
-	rtc = dot(rto, r->dir->val, r->dir->size);
-	h = pow(mag(rto, r->ori->size), 2) - pow(rtc, 2);
+	transform(r, sphere->t_matrix);
+	rto = sub(sphere->ori, r->ori);
+	rtc = dot(rto, r->dir);
+	h = pow(mag(rto), 2) - pow(rtc, 2);
 	if (sqrt(h) > sphere->rad)
-		return (free(rto), its(obj, NULL, 0));
+		return (free_t(rto), its(obj, NULL, 0));
 	result = calloc(2, sizeof(double));
 	if (!result)
-		return (free(rto), free(obj), NULL);
+		return (free_t(rto), free(obj), NULL);
 	h = sqrt(pow(sphere->rad, 2) - h);
 	result[0] = rtc - h;
 	result[1] = rtc + h;
 	if (result[0] == result[1])
-		return (free(rto), its(obj, result, 1));
-	return (free(rto), its(obj, result, 2));
+		return (free_t(rto), its(obj, result, 1));
+	return (free_t(rto), its(obj, result, 2));
 }
 
 void	transform(t_ray *ray, t_tuple **t_matrix)
