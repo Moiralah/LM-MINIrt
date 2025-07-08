93f5e6fb7b7d8bd8227256f97882baad93c378dd
MrNobody144 <ljshin15@gmail.com>
2025-07-08 19:52:05 +0800

    Added converter type for t_obj to get mat, ori and t_matrix attribute in most shape structures

--- notes/object.c
+++ notes/object.c
@@ -10,12 +10,34 @@ t_obj	*object(void *data, char type)
 	return (new_obj);
 }
 
-t_sphere	*sphere(t_tuple *origin, double radius)
+t_tuple	**get_obj_tf(t_obj *obj)
+{
+	if (obj->type == 'S')
+		return (((t_sphere *)(obj->data))->t_matrix);
+	return (NULL);
+}
+
+t_tuple	*get_obj_ori(t_obj *obj)
+{
+	if (obj->type == 'S')
+		return (((t_sphere *)(obj->data))->ori);
+	return (NULL);
+}
+
+t_mat	*get_obj_mat(t_obj *obj)
+{
+	if (obj->type == 'S')
+		return (((t_sphere *)(obj->data))->mat);
+	return (NULL);
+}
+
+t_sphere	*sphere(t_tuple *origin, t_mat *mat, double radius)
 {
 	t_sphere	*new_sphere;
 
 	new_sphere = calloc(1, sizeof(t_sphere));
 	new_sphere->t_matrix = identity(origin->size);
+	new_sphere->mat = mat;
 	new_sphere->ori = origin;
 	new_sphere->rad = radius;
 	return (new_sphere);
