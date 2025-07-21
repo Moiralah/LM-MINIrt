a3b9ab997fe2456bc66d3c895567e48bcb7d7afe
MrNobody144 <ljshin15@gmail.com>
2025-07-21 15:15:26 +0800

    Added copy_ray function that makes a new copy of a ray object

--- notes/ray.c
+++ notes/ray.c
@@ -13,79 +13,20 @@ t_ray	*ray(t_tuple *origin, t_tuple *direction)
 	return (new_ray);
 }
 
-// Calculates the position of a ray at a given time.
-t_tuple	*travel(t_ray *ray, double time)
+t_ray	*copy_ray(t_ray *old)
 {
-	t_tuple	*velo;
-	t_tuple	*new_pos;
+	t_ray	*new;
+	t_tuple	*new_ori;
+	t_tuple	*new_dir;
 
-	velo = mult(ray->dir, time);
-	new_pos = add(ray->ori, velo);
-	free_t(velo);
-	return (new_pos);
-}
-
-// Computes the intersections of a ray with a sphere.
-t_its	*sphere_its(t_ray *r, t_sphere *sphere)
-{
-	t_obj		*obj;
-	t_tuple		*rto;
-	double		*result;
-	double		rtc;
-	double		h;
-
-	obj = object(sphere, 'S');
-	if (!obj)
+	new_ori = copy_t(old->ori);
+	if (!new_ori)
 		return (NULL);
-	transform(r, sphere->t_matrix);
-	rto = sub(sphere->ori, r->ori);
-	rtc = dot(rto, r->dir);
-	h = pow(mag(rto), 2) - pow(rtc, 2);
-	if (sqrt(h) > sphere->rad)
-		return (free_t(rto), its(obj, NULL, 0));
-	h = pow(mag(rto), 2) - pow(rtc, 2);
-	result = calloc(2, sizeof(double));
-	if (!result)
-		return (free_t(rto), free(obj), NULL);
-	h = sqrt(pow(sphere->rad, 2) - h);
-	result[0] = rtc - h;
-	result[1] = rtc + h;
-	if (result[0] == result[1])
-		return (free_t(rto), its(obj, result, 1));
-	return (free_t(rto), its(obj, result, 2));
-}
-
-t_its	*intersect(t_ray *ray, t_obj *obj)
-{
-	if (obj->type == 'S')
-		return (sphere_its(ray, (t_sphere *)obj->data));
-	return (NULL);
-}
-
-void	transform(t_ray *ray, t_tuple **t_matrix)
-{
-	t_tuple	**temp_m;
-	t_tuple	**inverse_m;
-	t_tuple	**result_m;
-
-	temp_m = matrix(2, ray->ori);
-	if (!temp_m)
-		return ;
-	inverse_m = inverse(t_matrix);
-	if (!inverse_m)
-		return (free_m(temp_m, temp_m[0]->size));
-	result_m = mxm(temp_m, inverse_m);
-	ray->ori = result_m[0];
-	free_m(temp_m, temp_m[0]->size);
-	free(result_m);
-	temp_m = matrix(2, ray->dir);
-	if (!temp_m)
-		return (free_m(inverse_m, inverse_m[0]->size));
-	result_m = mxm(temp_m, inverse_m);
-	ray->dir = result_m[0];
-	free_m(temp_m, temp_m[0]->size);
-	free_m(inverse_m, inverse_m[0]->size);
-	free(result_m);
+	new_dir = copy_t(old->dir);
+	if (!new_dir)
+		return (NULL);
+	new = ray(new_ori, new_dir);
+	return (new);
 }
 
 // Frees the memory allocated for a ray.
