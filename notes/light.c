3d68471155d82d116bb40e16c6e73d13f27086fd
MrNobody144 <ljshin15@gmail.com>
2025-07-21 15:13:06 +0800

    Added copy_light function that makes a new copy of a light object

--- notes/light.c
+++ notes/light.c
@@ -22,6 +22,18 @@ t_light	*light(t_tuple *position, int intensity)
 	return (light);
 }
 
+t_light	*copy_light(t_light *old)
+{
+	t_light	*new;
+	t_tuple	*new_pos;
+
+	new_pos = copy_t(old->position);
+	if (!new_pos)
+		return (NULL);
+	new = light(new_pos, old->intensity);
+	return (new);
+}
+
 t_tuple	*world_to_obj_point(t_tuple **t_matrix, t_tuple *world_point)
 {
 	t_tuple	**inverse_m;
@@ -35,7 +47,7 @@ t_tuple	*world_to_obj_point(t_tuple **t_matrix, t_tuple *world_point)
 		return (NULL);
 	inverse_m = inverse(t_matrix);
 	if (!inverse_m)
-		return (free_m(point_m, point_m[0]->size), NULL);
+		return (free_m(point_m, len_m(point_m)), NULL);
 	obj_p = mxm(point_m, inverse_m);
 	if (obj_p)
 		result = obj_p[0];
@@ -62,7 +74,7 @@ t_tuple	*normal_at_obj(t_tuple **t_matrix, t_tuple *world_p, t_tuple *origin)
 	inverse_m = inverse(t_matrix);
 	if (!inverse_m)
 		return (free_m(obj_n, 2), free_t(obj_p), NULL);
-	transpose(inverse_m);
+	inverse_m = transpose(inverse_m);
 	world_n = mxm(obj_n, inverse_m);
 	if (!world_n)
 		return (free_m(obj_n, 2), free_m(inverse_m, 2), NULL);
@@ -72,13 +84,16 @@ t_tuple	*normal_at_obj(t_tuple **t_matrix, t_tuple *world_p, t_tuple *origin)
 }
 
 t_tuple	*reflect(t_tuple *in, t_tuple *normal)
-{
+{	
 	t_tuple	*result;
+	t_tuple	*temp;
 	double	dot_product;
 
 	dot_product = dot(in, normal);
-	result = sub(in, mult(normal, 2 * dot_product));
-	if (!result)
+	temp = mult(normal, 2.0 * dot_product);
+	if (!temp)
 		return (NULL);
+	result = sub(in, temp);
+	free_t(temp);
 	return (result);
 }
