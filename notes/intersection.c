537cb46d316d165d9878fe972a7b1ed3db45973a
MrNobody144 <ljshin15@gmail.com>
2025-07-27 08:41:16 +0800

    Fixed intersection functions to return a its list instead of just one its with multiple lengths

--- notes/intersection.c
+++ notes/intersection.c
@@ -33,41 +33,34 @@
 	return (free_t(rto), its(obj, result, 2));
 } */
 
-t_its	*calculate_its(t_ray *r, t_obj *obj, t_tuple *rto)
+t_its	**calculate_its(t_ray *r, t_obj *obj, t_tuple *rto)
 {
-	double		*result;
 	double		values[4];
+	double		result[2];
 
-	result = malloc(2 * sizeof(double));
-	if (!result)
-		return (NULL);
 	values[0] = dot(r->dir, r->dir);
 	values[1] = 2 * dot(r->dir, rto);
 	values[2] = dot(rto, rto) - 1;
 	values[3] = pow(values[1], 2) - (4 * values[0] * values[2]);
 	if (values[3] < 0)
-		return (free_t(rto), its(obj, NULL, 0));
+		return (free_t(rto), NULL);
 	result[0] = (-values[1] - sqrt(values[3])) / (2 * values[0]);
 	result[1] = (-values[1] + sqrt(values[3])) / (2 * values[0]);
 	if (result[0] == result[1])
-		return (free_t(rto), its(obj, result, 1));
-	return (free_t(rto), its(obj, result, 2));
+		return (free_t(rto), its_s(1, its(obj, result[0])));
+	return (free_t(rto), its_s(2, its(obj, result[0]), its(obj, result[1])));
 }
 
-t_its	*sphere_its(t_ray *r, t_sphere *sphere)
+t_its	**sphere_its(t_obj *obj, t_ray *r)
 {
-	t_obj		*obj;
 	t_ray		*new_ray;
 	t_tuple		**inverse_m;
 	t_tuple		*ray_values[2];
 	t_tuple		*rto;
 
-	obj = object(sphere, 'S');
-	if (!obj)
-		return (NULL);
-	inverse_m = inverse(sphere->t_matrix);
+	inverse_m = inverse(get_obj_tf(obj));
 	if (!inverse_m)
-		return (free(obj), NULL);
+		return (NULL);
 	ray_values[0] = transform_ori(inverse_m, r->ori);
 	if (!ray_values[0])
 		return (free_m(inverse_m, len_m(inverse_m)), free(obj), NULL);
@@ -77,8 +70,8 @@ t_its	*sphere_its(t_ray *r, t_sphere *sphere)
 	new_ray = ray(ray_values[0], ray_values[1]);
 	if (!new_ray)
 		return (free_m(inverse_m, len_m(inverse_m)), free(obj), NULL);
-	rto = sub(new_ray->ori, sphere->ori);
+	rto = sub(new_ray->ori, get_obj_ori(obj));
 	if (!rto)
-		return (free(obj), NULL);
+		return (NULL);
 	return (calculate_its(new_ray, obj, rto));
 }
