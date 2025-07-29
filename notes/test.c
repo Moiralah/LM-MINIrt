ac769dc24a459ead2996107e679d3317adfc1a96
MrNobody144 <ljshin15@gmail.com>
2025-07-29 19:51:13 +0800

    Added new test for shadow

--- notes/test.c
+++ notes/test.c
@@ -60,91 +60,6 @@
 	}
 } */
 
-// void	cast_rays(t_img *img, t_sphere *shape, t_light *l, t_tuple *ray_ori, double half, double pixel_size, double wall_z)
-// {
-// 	t_its	*xs;
-// 	t_ray	*r;
-// 	t_tuple	*colour;
-// 	t_tuple	*normal;
-// 	t_tuple	*pos;
-// 	t_tuple	*hit;
-// 	t_tuple	*eye;
-// 	double	world_y;
-// 	double	world_x;
-// 	int	hex;
-// 	int	y;
-// 	int	x;
-
-// 	y = -1;
-// 	//colour = rgb_hex(red->val[0], red->val[1], red->val[2]);
-// 	while (++y < img->w)
-// 	{
-// 		world_y = half - (pixel_size * y);
-// 		x = -1;
-// 		while (++x < img->h)
-// 		{
-// 			world_x = -half + (pixel_size * x);
-// 			pos = tuple(4, world_x, world_y, wall_z, 1.0);
-// 			r = ray(ray_ori, norm(sub(pos, ray_ori)));
-// 			xs = sphere_its(r, shape);
-// 			if (xs && xs->len)
-// 			{
-// 				hit = travel(r, xs->len[0]);
-// 				normal = normal_at_obj(get_obj_tf(xs->obj), hit, get_obj_ori(xs->obj));
-// 				eye = mult(r->dir, -1);
-// 				colour = lighting(get_obj_mat(xs->obj), l, matrix(4, hit, eye, normal));
-// 				hex = rgb_hex(colour->val[0], colour->val[1], colour->val[2]);
-// 				render_p(img, x, y, hex);
-// 			}
-// 			else
-// 				render_p(img, x, y, 0);
-// 		}
-// 	}
-// }
-
-// int	main(void)
-// {
-// 	t_img	img;
-// 	t_sphere	*s;
-// 	t_light	*l;
-// 	t_mat	*m;
-// 	t_tuple	*ray_ori;
-// 	t_tuple	*red;
-// 	void	*mlx;
-// 	void	*win;
-// 	double	pixel_size;
-// 	double	wall_size;
-// 	double	half;
-// 	int	canvas_pixels;
-// 	int	wall_z;
-
-// 	ray_ori = tuple(4, 0.0, 0.0, -5.0, 1.0);
-// 	wall_z = 10;
-// 	wall_size = 7.0;
-// 	canvas_pixels = 500;
-// 	img.w = canvas_pixels;
-// 	img.h = canvas_pixels;
-// 	pixel_size = wall_size / canvas_pixels;
-// 	half = wall_size / 2.0;
-// 	red = tuple(3, 1.0, 0.2, 1.0);
-// 	m = material(red, tuple(4, 0.1, 0.9, 0.9, 200.0));
-// 	s = sphere(tuple(4, 0.0, 0.0, 0.0, 1.0), m, 1.0);
-// 	s->t_matrix = mxm(rotate(4, 2, M_PI / 4.0), scale(4, 0.5, 1.0, 1.0));
-// 	s->t_matrix = mxm(translate(4, 0.2, 0.5, 0.0), s->t_matrix);
-// 	// s->t_matrix = shear(s->t_matrix, 0, 0.5, 0.5);
-// 	l = light(tuple(4, -10.0, 10.0, -10.0, 1.0), tuple(3, 1.0, 1.0, 1.0));
-// 	mlx = mlx_init();
-// 	win = mlx_new_window(mlx, img.w, img.h, "Sphere");
-// 	img.img = mlx_new_image(mlx, img.w, img.h);
-// 	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.len, &img.endian);
-// 	cast_rays(&img, s, l, ray_ori, half, pixel_size, wall_z);
-// 	mlx_put_image_to_window(mlx, win, img.img, 0, 0);
-// 	mlx_loop(mlx);
-// 	mlx_destroy_display(mlx);
-// 	free(mlx);
-// 	return (0);
-// }
-
 /* int	main(void)
 {
 	t_sphere	*s1;
@@ -252,7 +167,7 @@
 	print_t(colour);
 } */
 
-/* int	main(void)
+int	main(void)
 {
 	t_world		*w;
 	t_camera	*c;
@@ -267,8 +182,8 @@
 	int	hsize;
 
 
-	hsize = 1000;
-	vsize = 500;
+	hsize = 100;
+	vsize = 100;
 	img.w = hsize;
 	img.h = vsize;
 	from = tuple(4, 0.0, 1.5, -5.0, 1.0);
@@ -288,10 +203,15 @@
 	mlx_destroy_display(mlx);
 	free(mlx);
 	return (0);
-} */
-
-int	main(void)
-{
-	
-	return (0);
 }
+
+/* int	main(void)
+{
+	t_world	*w;
+	t_tuple	*p;
+
+	w = def_world();
+	p = tuple(4, 10.0, -10.0, 10.0, 1.0);
+	printf("%d\n", shadowed(w, p));
+	return (0);
+} */
