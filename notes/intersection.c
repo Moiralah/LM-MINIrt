e75de9215a9baa6855b446d0733b3a2990c6f870
MrNobody144 <ljshin15@gmail.com>
2025-08-01 11:01:14 +0800

    Added intersection function for cylinder shape

--- notes/intersection.c
+++ notes/intersection.c
@@ -33,11 +33,13 @@
 	return (free_t(rto), its(obj, result, 2));
 } */
 
-t_its	**calculate_its(t_ray *r, t_obj *obj, t_tuple *rto)
+t_its	**sphere_its(t_obj *obj, t_ray *r)
 {
+	t_tuple		*rto;
 	double		values[4];
 	double		result[2];
 
+	rto = sub(r->ori, get_obj_ori(obj));
 	values[0] = dot(r->dir, r->dir);
 	values[1] = 2 * dot(r->dir, rto);
 	values[2] = dot(rto, rto) - 1;
@@ -51,12 +53,63 @@ t_its	**calculate_its(t_ray *r, t_obj *obj, t_tuple *rto)
 	return (free_t(rto), its_s(2, its(obj, result[0]), its(obj, result[1])));
 }
 
-t_its	**sphere_its(t_obj *obj, t_ray *r)
+t_its	**check_cap(t_obj *obj, t_cylinder *cy, t_ray *r)
+{
+	t_its		**caps[2];
+	t_tuple		*hit;
+	double		t;
+
+	caps[0] = NULL;
+	caps[1] = NULL;
+	t = (cy->min - r->ori->val[1]) / r->dir->val[1]; 
+	hit = travel(r, t);
+	if ((pow(hit->val[0], 2) + pow(hit->val[2], 2)) <= 1)
+		caps[0] = its_s(1, its(obj, t));
+	t = (cy->max - r->ori->val[1]) / r->dir->val[1]; 
+	free_t(hit);
+	hit = travel(r, t);
+	if ((pow(hit->val[0], 2) + pow(hit->val[2], 2)) <= 1)
+		caps[1] = its_s(1, its(obj, t));
+	free_t(hit);
+	caps[0] = merge_its_s(caps[0], caps[1]);
+	return (caps[0]);
+}
+
+t_its	**cylinder_its(t_obj *obj, t_ray *r)
+{
+	t_its		**hits;
+	t_cylinder	*cy;
+	t_tuple		*hit[2];
+	double		values[4];
+	double		len[2];
+
+	cy = (t_cylinder *)(obj->data);
+	values[0] = pow(r->dir->val[0], 2) + pow(r->dir->val[2], 2);
+	if (values[0] == 0)
+		return (check_cap(obj, cy, r));
+	values[1] = (2 * r->ori->val[0] * r->dir->val[0]);
+	values[1] += (2 * r->ori->val[2] * r->dir->val[2]);
+	values[2] = pow(r->ori->val[0], 2) + pow(r->ori->val[2], 2) - 1;
+	values[3] = pow(values[1], 2) - (4 * values[0] * values[2]);
+	if (values[3] < 0)
+		return (NULL);
+	len[0] = (-values[1] - sqrt(values[3])) / (2 * values[0]);
+	len[1] = (-values[1] + sqrt(values[3])) / (2 * values[0]);
+	hit[0] = travel(r, len[0]);
+	hit[1] = travel(r, len[1]);
+	hits = check_cap(obj, cy, r);
+	if ((hit[0]->val[1] > cy->min) && (hit[0]->val[1] < cy->max))
+		hits = merge_its_s(its_s(1, its(obj, len[0])), hits);
+	if ((hit[1]->val[1] > cy->min) && (hit[1]->val[1] < cy->max))
+		hits = merge_its_s(its_s(2, its(obj, len[0]), its(obj, len[1])), hits);
+	return (hits);
+}
+
+t_its	**calculate_its(t_obj *obj, t_ray *r)
 {
 	t_ray		*new_ray;
 	t_tuple		**inverse_m;
 	t_tuple		*ray_values[2];
-	t_tuple		*rto;
 
 	inverse_m = inverse(get_obj_tf(obj));
 	if (!inverse_m)
@@ -70,8 +123,11 @@ t_its	**sphere_its(t_obj *obj, t_ray *r)
 	new_ray = ray(ray_values[0], ray_values[1]);
 	if (!new_ray)
 		return (free_m(inverse_m, len_m(inverse_m)), free(obj), NULL);
-	rto = sub(new_ray->ori, get_obj_ori(obj));
-	if (!rto)
-		return (NULL);
-	return (calculate_its(new_ray, obj, rto));
+	if (obj->type == 'S')
+		return (sphere_its(obj, new_ray));
+	else if (obj->type == 'P')
+		return (plane_its(obj, new_ray));
+	else if (obj->type == 'C')
+		return (cylinder_its(obj, new_ray));
+	return (NULL);
 }
