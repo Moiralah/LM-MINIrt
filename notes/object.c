bd033ff8f5ee5be104b4fc2dcca508300a04ae65
MrNobody144 <ljshin15@gmail.com>
2025-08-01 15:44:30 +0800

    Added rotate function to cylinder shape constructor

--- notes/object.c
+++ notes/object.c
@@ -66,8 +66,8 @@ t_obj	*sphere(t_tuple *origin, t_mat *mat, double radius)
 	w_ori = tuple(4, 0.0, 0.0, 0.0, 1.0);
 	move = sub(origin, w_ori);
 	new_sphere = calloc(1, sizeof(t_sphere));
-	temp[0] = translate(4, move->val[0], move->val[1], move->val[2]);
-	temp[1] = scale(4, radius, radius, radius);
+	temp[0] = scale(4, radius, radius, radius);
+	temp[1] = translate(4, move->val[0], move->val[1], move->val[2]);
 	new_sphere->t_matrix = mxm(temp[1], temp[0]);
 	free_m(temp[0], len_m(temp[0]));
 	free_m(temp[1], len_m(temp[1]));
@@ -78,7 +78,7 @@ t_obj	*sphere(t_tuple *origin, t_mat *mat, double radius)
 	return (obj);
 }
 
-t_obj	*cylinder(t_tuple *origin, t_mat *mat, double rad, double h)
+t_obj	*cylinder(t_tuple *origin, t_tuple *normal, t_mat *mat, t_tuple *dim)
 {
 	t_obj		*obj;
 	t_cylinder	*new_cy;
@@ -90,23 +90,29 @@ t_obj	*cylinder(t_tuple *origin, t_mat *mat, double rad, double h)
 	if (!w_ori)
 		return (NULL);
 	move = sub(origin, w_ori);
+	print_t(move);
 	if (!move)
 		return (NULL);
 	new_cy = calloc(1, sizeof(t_cylinder));
-	new_cy->t_matrix = translate(4, move->val[0], move->val[1], move->val[2]);
+	new_cy->t_matrix = rotate(4, 3, normal->val[0] * 90, normal->val[1] * 90, normal->val[2] * 90);
 	if (!new_cy->t_matrix)
 		return (NULL);
 	temp[0] = new_cy->t_matrix;
-	temp[1] = scale(4, rad, h, rad);
+	temp[1] = scale(4, dim->val[0], dim->val[1], dim->val[0]);
+	new_cy->t_matrix = mxm(temp[1], new_cy->t_matrix);
+	free_m(temp[0], len_m(temp[0]));
+	free_m(temp[1], len_m(temp[1]));
+	temp[0] = new_cy->t_matrix;
+	temp[1] = translate(4, move->val[0], move->val[1], move->val[2]);
 	new_cy->t_matrix = mxm(temp[1], new_cy->t_matrix);
 	free_m(temp[0], len_m(temp[0]));
 	free_m(temp[1], len_m(temp[1]));
 	new_cy->mat = mat;
 	new_cy->ori = w_ori;
-	new_cy->rad = rad;
+	new_cy->rad = dim->val[0];
 	new_cy->max = 1.0; 
 	new_cy->min = -1.0;
-	new_cy->h = h;
+	new_cy->h = dim->val[1];
 	new_cy->closed = 1;
 	obj = object(new_cy, 'C');
 	return (obj);
